4ef588eb0bee2f4d40d1c557909435dd806b7130
PerrigThiago <thiagoperrig7@gmail.com>
2026-03-11 10:17:42 -0300

    comienzo del componente hostorialturnos

--- fullstack/frontend/src/App.jsx
+++ fullstack/frontend/src/App.jsx
@@ -1,41 +1,11 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import HistorialTurnos from "./components/HistorialTurnos";
 
-/*
-  useState crea un estado dentro del componente
-  turnos: valor actual
-  setTurnos: función para actualizarlo
-
-  turnos.map(...) recorre el array y pinta un <li> por cada turno
-  key={t.id} ayuda a React a identificar cada elemento de la lista
-*/
 function App() {
-  const [turnos, setTurnos] = useState([
-    { id: 1, nombre: "Emanuel", numero: 1 },
-    { id: 2, nombre: "Thiago", numero:2 },
-  ]);
-
-  useEffect(() => {
-    fetch("http://localhost:3000/api/turnos")
-      .then((res) => res.json())
-      .then((data) => setTurnos(data))
-      .catch((err) => {
-        console.error("Error al cargar turnos", err);
-      });
-    }, 
-  []);
-
-return (
-  <div>
-    <h1>Turnero</h1>
-    <ul>
-      {turnos.map((t) => (
-        <li key={t.id}>
-          #{t.numero} - {t.nombre}
-        </li>
-      ))}
-    </ul>
-  </div>
+  return (
+    <div>
+      <h1>Turnero</h1>
+      <HistorialTurnos />
+    </div>
   );
 }
 
