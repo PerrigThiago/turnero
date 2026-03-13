dc425b9afcb64aad9159fd6f7a568a57d6e3c263
Emanuel Botto <emanuelbotto25@gmail.com>
2026-03-13 17:27:50 -0300

    solucione el problema de eliminar turno

--- fullstack/backend/models/turno.js
+++ fullstack/backend/models/turno.js
@@ -42,8 +42,17 @@ export const updateTurno = async (id, detalle) => {
 };
 
 export const deleteTurno = async (id) => {
+
+  // borrar usuario relacionado
+  await pool.query(
+    "DELETE FROM usuario WHERE id_turno = $1",
+    [id]
+  );
+
+  // borrar turno
   await pool.query(
     "DELETE FROM turno WHERE id = $1",
     [id]
   );
 };
+
